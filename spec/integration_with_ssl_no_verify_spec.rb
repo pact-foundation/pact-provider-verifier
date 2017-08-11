66c67fde356218ab4f2367bb7d3a3fd677a5115e
Beth Skurrie <beth@bethesque.com>
2017-08-11 12:19:13 +1000

    test(ssl): Try a different port for ssl server to run on

--- spec/integration_with_ssl_no_verify_spec.rb
+++ spec/integration_with_ssl_no_verify_spec.rb
@@ -3,12 +3,12 @@ require 'support/provider_with_self_signed_cert'
 describe "verifying a provider that uses a self signed certificate" do
   before(:all) do
     @ssl_server_pid = fork do
-      run_provider_with_self_signed_cert 4568
+      run_provider_with_self_signed_cert 8080
     end
     sleep 2
   end
 
-  subject { `bundle exec bin/pact-provider-verifier -a 1.0.0 --provider-base-url https://localhost:4568 --pact-urls ./test/me-they.json --provider_states_setup_url https://localhost:4568/provider-state -v` }
+  subject { `bundle exec bin/pact-provider-verifier -a 1.0.0 --provider-base-url https://localhost:8080 --pact-urls ./test/me-they.json --provider_states_setup_url https://localhost:8080/provider-state -v` }
 
   it "passes because it has SSL verification turned off" do
     expect(subject).to include "2 interactions, 0 failures"
