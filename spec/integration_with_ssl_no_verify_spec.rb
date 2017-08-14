71f36b2410e438e8cd1c00ec822d6e0b0291b109
Beth <beth@bethesque.com>
2017-08-14 11:09:14 +1000

    chore(ssl server): start server in it block rather than before all
    Still getting travis problems

--- spec/integration_with_ssl_no_verify_spec.rb
+++ spec/integration_with_ssl_no_verify_spec.rb
@@ -3,21 +3,17 @@ require 'find_a_port'
 
 describe "verifying a provider that uses a self signed certificate" do
 
-  before(:all) do
-    @port = FindAPort.available_port
-    @ssl_server_pid = fork do
-      run_provider_with_self_signed_cert @port
-    end
-    sleep 2
-  end
-
-  subject { `bundle exec bin/pact-provider-verifier -a 1.0.0 --provider-base-url https://localhost:#{@port} --pact-urls ./test/me-they.json --provider_states_setup_url https://localhost:#{@port}/provider-state -v` }
-
   it "passes because it has SSL verification turned off" do
-    expect(subject).to include "2 interactions, 0 failures"
-  end
-
-  after(:all) do
-    Process.kill('KILL', @ssl_server_pid)
+    begin
+      @port = FindAPort.available_port
+      @ssl_server_pid = fork do
+        run_provider_with_self_signed_cert @port
+      end
+      sleep 2
+      subject = `bundle exec bin/pact-provider-verifier -a 1.0.0 --provider-base-url https://localhost:#{@port} --pact-urls ./test/me-they.json --provider_states_setup_url https://localhost:#{@port}/provider-state -v`
+      expect(subject).to include "2 interactions, 0 failures"
+    ensure
+      Process.kill('KILL', @ssl_server_pid)
+    end
   end
 end
