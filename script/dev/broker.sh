72b054154c30a849f195d59a27eef1f58f30bf63
Beth Skurrie <beth@bethesque.com>
2019-11-10 12:41:22 +1100

    chore: commit verification script that uses test broker

--- script/dev/broker.sh
+++ script/dev/broker.sh
@@ -3,15 +3,18 @@ pid=$!
 sleep 3
 
 export PACT_BROKER_PACTS_FOR_VERIFICATION_ENABLED=true
-# bundle exec bin/pact-provider-verifier --provider Bar --consumer-version-tag prod --pact-broker-base-url http://localhost:9292 -a 1.0.100 --provider-base-url http://localhost:4567 --provider-states-setup-url http://localhost:4567/provider-state
-  # --consumer-version-tag dev \
-bundle exec bin/pact-provider-verifier --broker-token localhost \
+
+bundle exec bin/pact-provider-verifier  \
+  --broker-username dXfltyFMgNOFZAxr8io9wJ37iUpY42M \
+  --broker-password O5AIZWxelWbLvqMd8PkAVycBJh2Psyg1 \
   --provider Bar \
   --provider-version-tag pdev \
-  --pact-broker-base-url http://localhost:9292 \
-  -a 1.0.100 --provider-base-url http://localhost:4567 \
+  --pact-broker-base-url https://test.pact.dius.com.au \
+  -a 1.0.100 \
+  --provider-base-url http://localhost:4567 \
   --provider-states-setup-url http://localhost:4567/provider-state \
-  --publish-verification-results
+  --publish-verification-results \
+  --format progress
 
 kill -2 $pid
 wait $pid
