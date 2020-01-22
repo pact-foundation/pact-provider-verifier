ad588b7718b3a1bed7096bdb0174ee5beccdabc4
Beth Skurrie <beth@bethesque.com>
2020-01-22 13:25:50 +1100

    chore: add URL of travis build to release script [ci-skip]

--- script/release.sh
+++ script/release.sh
@@ -6,3 +6,4 @@ bundle exec rake generate_changelog
 git add CHANGELOG.md lib/pact/provider_verifier/version.rb tasks/package.rake
 git commit -m "chore: release $(bundle exec ruby -e "require 'rake'; load 'tasks/package.rake'; puts VERSION")" && git push
 bundle exec rake tag_for_release
+echo "Releasing from https://travis-ci.org/pact-foundation/pact-provider-verifier"
