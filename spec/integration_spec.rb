2e3dc4cc64150673da142ab49517a6a5729519fe
Beth Skurrie <beth@bethesque.com>
2020-06-24 08:39:47 +1000

    test: more waiting

--- spec/integration_spec.rb
+++ spec/integration_spec.rb
@@ -141,6 +141,7 @@ describe "pact-provider-verifier" do
     it "the logs are written at the right level" do
       subject
       Process.kill 'INT', @pipe.pid
+      sleep 5
       expect(File.exist?('tmp/logs/pact.log'))
       logs = File.read('tmp/logs/pact.log')
       expect(logs).to include ('INFO')
