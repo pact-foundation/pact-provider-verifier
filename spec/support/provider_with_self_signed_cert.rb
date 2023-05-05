9ac818f19ac0325dbf326f8ef1b71d5b5bdb48ee
Yousaf Nabi <you@saf.dev>
2023-05-05 14:33:44 +0100

    fix(deps): rack 2.2.6 + ruby 3 change to WEBrick.run for args

--- spec/support/provider_with_self_signed_cert.rb
+++ spec/support/provider_with_self_signed_cert.rb
@@ -7,8 +7,16 @@ def run_provider_with_self_signed_cert port
   require 'webrick/https'
 
   webrick_opts = {:Port => port, :SSLEnable => true, :SSLCertName => [%w[CN localhost]]}
-  Rack::Handler::WEBrick.run(Provider, webrick_opts) do |server|
-    @server = server
+  # https://www.rubydoc.info/gems/rack/2.2.6/Rack%2FHandler%2FWEBrick.run
+  if RUBY_VERSION < "3"
+    # To work with Ruby 2.7.0, this needs an explicit ruby2_keywords
+    Rack::Handler::WEBrick.run(Provider, webrick_opts) do |server|
+      @server = server
+    end
+  else
+    Rack::Handler::WEBrick.run(Provider, **webrick_opts) do |server|
+      @server = server
+    end
   end
 end
 
